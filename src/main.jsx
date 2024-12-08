d7da1c68bf98d4b46e71f17ad994fb91f8cfe56c
TONYSTARKOP <msonu2018@gmail.com>
2024-12-08 16:33:00 +0530

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'styles.css'
 import App from '/app.jsx'
 
 createRoot(document.getElementById('root')).render(
